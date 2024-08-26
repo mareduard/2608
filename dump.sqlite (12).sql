d775160f529a99f4cb9684c7f5e8817146375653
mareduard <159054835+mareduard@users.noreply.github.com>
2024-08-26 15:35:52 -0300

    Add files via upload

---
+++ dump.sqlite (12).sql
@@ -0,0 +1,18 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE professores(
+  id_professor INTEGER PRIMARY KEY AUTOINCREMENT,
+  nome TEXT,
+  id_sala INTEGER, FOREIGN KEY (id_sala) REFERENCES salas(id_sala)
+  );
+CREATE TABLE salas(
+  id_sala INTEGER PRIMARY KEY AUTOINCREMENT,
+  numero_sala TEXT
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
